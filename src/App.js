e501753d53451a7c72a4386b64f582bd049c40b7
Daria <Plekhanova.danya@gmail.com>
2021-03-30 13:25:11 +0300

    На "страницы" добавлен контент.

--- src/App.js
+++ src/App.js
@@ -5,44 +5,58 @@ import {Route, BrowserRouter, NavLink} from "react-router-dom";
 function Menu() {
     return(
         <nav className="nav">
-            <NavLink className="nav-link active" aria-current="page" to="/">Главная</NavLink>
-            <NavLink className="nav-link" to="/about">О нас</NavLink>
-            <NavLink className="nav-link" to="/contact-us">Контакты</NavLink>
+            <NavLink className="nav-link active" aria-current="page" to="/">ГЛАВНАЯ</NavLink>
+            <NavLink className="nav-link" to="/about">О НАС</NavLink>
+            <NavLink className="nav-link" to="/contact-us">КОНТАКТЫ</NavLink>
         </nav>
     );
 }
 
 function Main() {
     return(
-        <h4>Главная страница</h4>
-    );
-}
-
-function ContactUs() {
-    return(
-
-        <div className = "container my-5">
-            <form action="">
-                <div className="mb-3">
-                    <input type="text" className="form-control"/>
-                </div>
-                <div className="mb-3">
-                    <input type="text" className="form-control"/>
-                </div>
-                <div className="mb-3">
-                    <textarea type="text" className="form-control"/>
-                </div>
-                <div className="mb-3">
-                    <input type="submit" className="form-control btn btn-primary"/>
-                </div>
-            </form>
+        <div>
+            <h4 className="mb-5">Главная страница</h4>
+            <div className="container">
+                <img src="..." className="img-fluid img-thumbnail bg-light p-5" alt="изображение" />
+            </div>
         </div>
     );
 }
 
 function AboutUs() {
     return(
-        <h4>О нас</h4>
+        <div>
+            <h4 className="mb-5">О нас</h4>
+            <div className="container text-start">
+                <p>Телефон: +7 (800) 700-89-89</p>
+                <p>E-mail: tel800@mail.ru</p>
+                <p>Время работы: ПН-ПТ: 09:00-18:00</p>
+            </div>
+        </div>
+    );
+}
+
+function ContactUs() {
+    return(
+        <div>
+            <h4 className="mb-5">Обратная связь</h4>
+            <div className = "container">
+                <form action="">
+                    <div className="mb-3">
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="mb-3">
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="mb-3">
+                        <textarea type="text" className="form-control"/>
+                    </div>
+                    <div className="mb-3">
+                        <input type="submit" className="form-control btn btn-primary"/>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
 
