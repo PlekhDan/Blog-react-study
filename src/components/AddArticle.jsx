f11a69439d57711233607dfcc9e51ca37cdf7351
Daria <Plekhanova.danya@gmail.com>
2021-04-05 18:45:19 +0300

    Редирект после публикации стати на главную страницу. Отправляем на сервер форму с новой статьей только, если info == "". Кнопка отправки новой статьи не активна, пока не введен уникальный заголовок статьи. Регистр не учитывается. Кнопка заблокирована, если ничего не введено в поле ввода заголовка статьи.

--- src/components/AddArticle.jsx
+++ src/components/AddArticle.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Redirect} from "react-router-dom";
 
 export class AddArticle extends React.Component {
     constructor(props) {
@@ -9,25 +10,31 @@ export class AddArticle extends React.Component {
             title: "",
             text: "",
             author: "",
-            info: ""
+            info: "",
+            redirect: false,
+            submitBtn: "disabled"
         };
     }
 
     // Отправка на сервер полученных данных.
     handleSubmit(event) {
         event.preventDefault();            // переопределение стандартного поведения браузера при отправке формы: перезагрузка страницы.
-        console.log("Отправка формы");
-        const formData = new FormData();
-        formData.append("title", this.state.title);
-        formData.append("text", this.state.text);
-        formData.append("author", this.state.author);
-        fetch("http://p9152834.beget.tech/php/addArticle.php", {
-            method: "POST",
-            body: formData
-        }).then(response => response.json())
-            .then(result => {
-                console.log(result);
-            });
+        if (this.state.info === "") {
+            const formData = new FormData();
+            formData.append("title", this.state.title);
+            formData.append("text", this.state.text);
+            formData.append("author", this.state.author);
+            fetch("http://p9152834.beget.tech/php/addArticle.php", {
+                method: "POST",
+                body: formData
+            }).then(response => response.json())
+                .then(result => {
+                    this.setState({
+                            redirect: true
+                        }
+                    )
+                });
+        }
     }
 
     // Обработчик, который реагирует на изменение input.
@@ -36,7 +43,11 @@ export class AddArticle extends React.Component {
         const name = event.target.name;
         const value = event.target.value;
         if (name === "title") {
-            console.log("Отправляю запрос ...");
+            if (value === "") {
+                this.setState( {
+                    submitBtn: "disabled"
+                })
+            }
             const formData = new FormData();
             formData.append("title", value);
             fetch("http://p9152834.beget.tech/php/checkTitle.php", {
@@ -46,11 +57,13 @@ export class AddArticle extends React.Component {
                 .then(result => {
                     if (result.result === "exist") {
                         this.setState({
-                            info: "Статья с таким заголовком уже существует"
+                            info: "Статья с таким заголовком уже существует",
+                            submitBtn: "disabled"
                         });
                     } else {
                         this.setState({
-                            info: ""
+                            info: "",
+                            submitBtn: ""
                         });
                     }
                 });
@@ -62,29 +75,33 @@ export class AddArticle extends React.Component {
     }
 
     render() {
-        return (
-            <div className="col-md-5 my-5 mx-auto">
-                <h4 className="mb-5">Новая статья</h4>
-                <form onSubmit={this.handleSubmit}>
-                    <div className="mb-3">
-                        <input name="title" value={this.state.title} onChange={this.handleInputChange} type="text"
-                               className="form-control"
-                               placeholder="Заголовок статьи"/>
-                        <p style={{color: "red"}}>{this.state.info}</p>
-                    </div>
-                    <div className="mb-3">
+        if (this.state.redirect) {
+            return (<Redirect to="/"/>);
+        } else {
+            return (
+                <div className="col-md-5 my-5 mx-auto">
+                    <h4 className="mb-5">Новая статья</h4>
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="mb-3">
+                            <input name="title" value={this.state.title} onChange={this.handleInputChange} type="text"
+                                   className="form-control"
+                                   placeholder="Заголовок статьи"/>
+                            <p style={{color: "red"}}>{this.state.info}</p>
+                        </div>
+                        <div className="mb-3">
                         <textarea name="text" value={this.state.text} onChange={this.handleInputChange} type="text"
                                   className="form-control" placeholder="Текст статьи"/>
-                    </div>
-                    <div className="mb-3">
-                        <input name="author" value={this.state.author} onChange={this.handleInputChange} type="text"
-                               className="form-control" placeholder="Автор"/>
-                    </div>
-                    <div className="mb-3">
-                        <input type="submit" className="form-control btn btn-primary" value="Опубликовать"/>
-                    </div>
-                </form>
-            </div>
-        );
+                        </div>
+                        <div className="mb-3">
+                            <input name="author" value={this.state.author} onChange={this.handleInputChange} type="text"
+                                   className="form-control" placeholder="Автор"/>
+                        </div>
+                        <div className="mb-3">
+                            <input type="submit" disabled={this.state.submitBtn} className="form-control btn btn-primary" value="Опубликовать"/>
+                        </div>
+                    </form>
+                </div>
+            );
+        }
     }
 }
